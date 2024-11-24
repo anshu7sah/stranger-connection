50ce25ddbcd9905e870434921a18a8ee7058395f
Anshu Sah <anshu7sah@gmail.com>
2024-11-25 01:30:23 +0545

    checked coins and divert the user to connecting activities for finding new user for calling and given permission script in the manifest

--- app/src/main/java/com/example/strangerconnection/activities/MainActivity.java
+++ app/src/main/java/com/example/strangerconnection/activities/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.strangerconnection.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,13 +15,20 @@ import androidx.core.view.WindowInsetsCompat;
 import com.bumptech.glide.Glide;
 import com.example.strangerconnection.R;
 import com.example.strangerconnection.databinding.ActivityMainBinding;
+import com.example.strangerconnection.models.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     FirebaseAuth auth;
+    FirebaseDatabase database;
+    long coins=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +38,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         auth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+
         FirebaseUser currentUser=auth.getCurrentUser();
+
+        database.getReference().child("profiles")
+                        .child(currentUser.getUid())
+                                .addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        User user=snapshot.getValue(User.class);
+                                        coins=user.getCoins();
+                                        binding.coins.setText("You have: "+coins);
+                                        Glide.with(MainActivity.this).load(user.getProfile()).into(binding.profilePicture);
+
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Glide.with(this).load(currentUser.getPhotoUrl()).into(binding.profilePicture);
+        binding.findButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(coins>5){
+                    startActivity(new Intent(MainActivity.this,ConnectingActivity.class));
+                }else{
+                    Toast.makeText(MainActivity.this, "Insufficient Coins", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
